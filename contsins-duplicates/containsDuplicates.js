0d9fbcad7274ab78f42ffb72a4483964ddd23050
Ahsen Ayaz <ahsen.ayaz@datumbrain.com>
2024-03-29 22:39:16 +0500

    :white_check_mark: Solve ContainDuplicates Problem

--- contsins-duplicates/containsDuplicates.js
+++ contsins-duplicates/containsDuplicates.js
@@ -3,13 +3,24 @@
  * @return {boolean}
  */
 var containsDuplicate = function (nums) {
+  nums.sort((a, b) => a - b);
   for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] === nums[j]) {
-        return true;
-      } else {
-        return false;
-      }
+    if (nums[i] === nums[i + 1]) {
+      return true;
     }
   }
+  return false;
 };
+
+// Also the Possible Solution
+
+// var containsDuplicate = function (nums) {
+// for (let i = 0; i < nums.length; i++) {
+//   for (let j = i + 1; j < nums.length; j++) {
+//     if (nums[i] === nums[j]) {
+//       return true;
+//     }
+//   }
+// }
+// return false;
+// };
